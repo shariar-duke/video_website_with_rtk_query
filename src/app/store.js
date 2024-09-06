0667a3d7ed1d09c9706e3edcbfdced9db3d78fdc
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-06 15:14:39 +0600

    api slice and store modified

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import filterReducer from "../features/filter/filterSlice";
-import relatedVideoReducer from "../features/relatedVideos/relatedVideosSlice";
-import tagReducer from "../features/tags/tagsSlice";
-import singleVideoReducer from "../features/video/videoSlice";
-import videoReducer from "../features/videos/videosSlice";
-
+import { apiSlice } from "../features/api/apiSlice";
 export const store = configureStore({
     reducer: {
-        videos: videoReducer,
-        tags: tagReducer,
-        video: singleVideoReducer,
-        relatedVideos: relatedVideoReducer,
-        filters : filterReducer
+      [apiSlice.reducerPath] : apiSlice.reducer
+    },
+    middleware:(getDefaultMiddlewares)=>{
+       getDefaultMiddlewares().concat(apiSlice.middleware)
     }
 })
