7fae9c8b8c5e25a282c4eb7589238e00a314c5b6
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-07 18:05:47 +0600

    data fetched in RelatedVideoList component

--- src/components/list/RelatedVideoList.jsx
+++ src/components/list/RelatedVideoList.jsx
@@ -1,40 +1,49 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchRelatedVideo } from "../../features/relatedVideos/relatedVideosSlice";
-import Loading from "../ui/Loading";
+
+import { useGetRelatedVideosQuery } from "../../features/api/apiSlice";
+import RelatedVideoLoader from "../ui/RelatedVideoLoader";
 import RelatedVideoListItem from "./RelatedVideoListItem";
-export default function RelatedVideoList({currentVideoId ,tags }) {
-
-  const {relatedVideos, isLoading, isError, error} = useSelector((state) => state.relatedVideos)
-  console.log("The related videos are", relatedVideos)
-  const dispatch = useDispatch()
-  useEffect(()=> {
-   dispatch(fetchRelatedVideo({id:currentVideoId , tags}))
-  },[dispatch, tags, currentVideoId])
-
-  // decide which one will render 
+export default function RelatedVideoList({ id, title }) {
+  const {
+    data: relatedVideos,
+    isLoading,
+    isError,
+  } = useGetRelatedVideosQuery({ id, title });
+  // decide which one will render
   let content;
   if (isLoading) {
-    content = <Loading />;
+    content = (
+      <>
+        <RelatedVideoLoader />
+        <RelatedVideoListItem />
+        <RelatedVideoLoader />
+        <RelatedVideoListItem />
+        <RelatedVideoLoader />
+        <RelatedVideoListItem />
+      </>
+    );
   }
 
   if (!isLoading && isError) {
-    content = <div className="col-span-12">{error}</div>;
+    content = (
+      <div className="col-span-12 text-[20px] text-red-500">
+        <p>No Related videos</p>
+      </div>
+    );
   }
 
-  if(!isError && !isLoading && relatedVideos?.length == 0) 
-    {
-      content = <div className="col-span-12">No Videos Found</div>;
-    }
-  if(!isError && !isLoading && relatedVideos?.length > 0) 
-  {
-    content = relatedVideos.map((video) => <RelatedVideoListItem key={video.id} video={video}/> ) 
+  if (!isError && !isLoading && relatedVideos?.length == 0) {
+    content = <div className="col-span-12">No Videos Found</div>;
+  }
+  if (!isError && !isLoading && relatedVideos?.length > 0) {
+    content = relatedVideos.map((video) => (
+      <RelatedVideoListItem key={video.id} video={video} />
+    ));
   }
 
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-     {content}
+      {content}
     </div>
   );
 }
