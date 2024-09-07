dc9ad84a14f468c686af7763b3a4af5866015a26
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-07 10:09:56 +0600

    error fix in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
+
 export const store = configureStore({
-    reducer: {
-      [apiSlice.reducerPath] : apiSlice.reducer
-    },
-    middleware:(getDefaultMiddlewares)=>{
-       getDefaultMiddlewares().concat(apiSlice.middleware)
-    }
-})
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
