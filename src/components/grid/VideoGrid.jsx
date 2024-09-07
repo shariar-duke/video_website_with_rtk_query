038991e25f0ae437d66c8d5a92f0b0c9bf5e0558
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-07 10:56:50 +0600

    Data fetched with hooks in VideoGrid.jsx

--- src/components/grid/VideoGrid.jsx
+++ src/components/grid/VideoGrid.jsx
@@ -1,49 +1,45 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchVideos } from "../../features/videos/videosSlice";
+import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Loading from "../ui/Loading";
 import VideoGridItem from "./VideoGridItem";
 export default function VideoGrid() {
-  const dispatch = useDispatch();
-
-
-
-  // state takeo amader lagbe
-
-  const { videos, isLoading, isError, error } = useSelector(
-    (state) => state.videos
-  );
-
-  const {tags, search} = useSelector((state)=> state.filters)
-  useEffect(() => {
-    dispatch(fetchVideos({tags, search}));
-  }, [dispatch, tags, search]);
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
 
   // deciding what to render error message ta dekhabo naki videos gula dekhabo. videos thakle videos gula dekhabo. r jode videos na ase tahle error message gula dekhabo
 
-  let content;
+  let content = null;
   if (isLoading) {
-    content = <Loading />;
+    content = (
+      <>
+        <Loading />
+        <Loading />
+        <Loading />
+      </>
+    );
   }
 
   if (!isLoading && isError) {
-    content = <div className="col-span-12">{error}</div>;
+    content = (
+      <div className="col-span-12 text-red-500">
+        <p>Error Loading Videos</p>{" "}
+      </div>
+    );
   }
 
-  if(!isError && !isLoading && videos?.length == 0) 
-    {
-      content = <div className="col-span-12">No Videos Found</div>;
-    }
-  if(!isError && !isLoading && videos?.length > 0) 
-  {
-    content = videos.map((video) => <VideoGridItem key={video.id} video={video}/> ) 
+  if (!isError && !isLoading && videos?.length == 0) {
+    content = <div className="col-span-12">No Videos Found</div>;
+  }
+  if (!isError && !isLoading && videos?.length > 0) {
+    content = videos.map((video) => (
+      <VideoGridItem key={video.id} video={video} />
+    ));
   }
 
   return (
     <section className="pt-2">
       <section className="pt-2">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-         {content}
+           {content} 
+       
         </div>
       </section>
     </section>
