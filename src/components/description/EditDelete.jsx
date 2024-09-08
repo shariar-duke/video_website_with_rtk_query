76824e019b4596594fe0761bc6c7a6af66257f6d
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-08 11:30:56 +0600

    add dynamic link  for going Edit Form Pgae

--- src/components/description/EditDelete.jsx
+++ src/components/description/EditDelete.jsx
@@ -1,21 +1,21 @@
-import { FaEdit, FaTrash } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-export default function EditDelete() {
+/* eslint-disable react/prop-types */
+import { FaEdit, FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function EditDelete({id}) {
   return (
     <div className="flex space-x-4">
-    {/* Edit Button */}
-    <Link
-  to="/edit/1"
-  className="flex items-center bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded hover:bg-gray-300"
->
-  <FaEdit className="mr-2" /> Edit
-</Link>
-  
-    
-    {/* Delete Button */}
-    <button className="flex items-center bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded hover:bg-gray-300">
-      <FaTrash className="mr-2" /> Delete
-    </button>
-  </div>
+      {/* Edit Button */}
+      <Link
+        to={`/edit/${id}`}
+        className="flex items-center bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded hover:bg-gray-300"
+      >
+        <FaEdit className="mr-2" /> Edit
+      </Link>
+
+      {/* Delete Button */}
+      <button className="flex items-center bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded hover:bg-gray-300">
+        <FaTrash className="mr-2" /> Delete
+      </button>
+    </div>
   );
 }
